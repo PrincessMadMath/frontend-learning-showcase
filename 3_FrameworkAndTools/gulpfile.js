9326855c35b5f2b886373ee982da663e7d0ff519
Mathieu <math.gamache@gmail.com>
2016-10-03 22:47:40 -0400

    Migration to Gulp 4.0 + clean up of gulp file + auto-format of css

--- 3_FrameworkAndTools/gulpfile.js
+++ 3_FrameworkAndTools/gulpfile.js
@@ -3,67 +3,91 @@ var sass = require("gulp-sass");
 var browserSync = require("browser-sync").create();
 var cleanCSS = require('gulp-clean-css');
 var del = require("del");
-var runSequence = require("run-sequence");
 
+var postcss = require('gulp-postcss');
+var cssnano = require('cssnano');
+var autoprefixer = require('autoprefixer');
+var reporter = require('postcss-reporter');
+
+var syntax_scss = require('postcss-scss');
+var stylelint = require('stylelint');
+
+var sorting = require('postcss-sorting');
+
+
+gulp.task('clean:dist', function(done) {
+    return del(['dist/*'], done);
+});
+
+gulp.task("build", gulp.series('clean:dist', format, lint_css, gulp.parallel(process_css, process_html)));
 
 // Before: compile lastest version of saas + setup browser-sync server
 // Watch for update in .scss files and call sass compile if it is the case
-gulp.task("watch", ["browser-sync", "sass-compile"], function() {
-    gulp.watch("./app/scss/**/*.scss", ["sass-compile"]);
-    gulp.watch("./app/scss/**/*.css", ["sass-compile"]);
-    gulp.watch("./app/*.html", browserSync.reload);
+gulp.task("watch", gulp.series(gulp.parallel(init_browser_sync, "build", watch)));
+
+function watch() {
+
+    var scss_watcher = gulp.watch("./app/scss/**/*.scss");
+    scss_watcher.on("all", function(event, path, stats) {
+        console.log('File ' + path + ' was ' + event + ', running tasks...');
+        format();
+        sync_css();
+    });
+
+    var html_watch = gulp.watch("./app/*.html", gulp.series(process_html, browserSync.reload));
+};
+
+gulp.task("watch:test", function() {
+    var scss_watcher = gulp.watch("./app/scss/**/*.scss");
+
+    console.log('Start watching...');
+    scss_watcher.on("all", function(event, path, stats) {
+        console.log('File ' + path + ' was ' + event + ', running tasks...');
+        sync();
+        console.log('task done....');
+    });
+
 });
 
+gulp.task('lint-css', lint_css);
+gulp.task('format', format);
 
-gulp.task("build", function(callback) {
-    runSequence('clean:dist', ['sass-compile', 'copy-html'], 'minify-css', callback)
-    console.log('Building files');
-});
-
-
-// Browser-sunc will setup a server from the root we give it
-gulp.task("browser-sync", function() {
+function init_browser_sync() {
     browserSync.init({
         server: {
-            baseDir: "app"
+            baseDir: './dist'
         }
     });
-});
+}
 
-// Compile the scss file and output in app/css
-gulp.task("sass-compile", function() {
-    return gulp.src("./app/scss/main.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("./app/css"))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
-
-// Minify the css
-gulp.task('minify-css', function() {
-    return gulp.src("./app/css/*.css")
-        .pipe(cleanCSS({
-            debug: true
-        }, function(details) {
-            console.log(details.name + ': ' + details.stats.originalSize);
-            console.log(details.name + ': ' + details.stats.minifiedSize);
-        }))
-        .pipe(gulp.dest('./dist/css'));
-})
+function sync_css() {
+    return process_css()
+        .pipe(browserSync.stream());
+}
 
 
-gulp.task('clean:dist', function() {
-    return del.sync('dist/*');
-})
 
-gulp.task('copy-html', function() {
+function process_html() {
     return gulp.src('./app/*.html')
         .pipe(gulp.dest('dist'));
-});
+}
 
+function process_css() {
 
-gulp.task('lint-css', function() {
+    var processors = [
+        autoprefixer({
+            browsers: ['last 2 versions']
+        }),
+        cssnano
+    ];
+
+    return gulp.src("./app/scss/main.scss")
+        .pipe(sass())
+        .pipe(postcss(processors))
+        .pipe(gulp.dest("dist/css"));
+}
+
+function lint_css() {
     const gulpStylelint = require('gulp-stylelint');
 
     return gulp.src('./app/scss/*.scss')
@@ -73,5 +97,19 @@ gulp.task('lint-css', function() {
                 console: true
             }]
         }));
+}
 
-});
+function format() {
+    var stylefmt = require('stylefmt');
+
+    var processors = [
+        sorting,
+        stylefmt
+    ];
+
+    return gulp.src("./app/scss/main.scss")
+        .pipe(postcss(processors, {
+            syntax: syntax_scss
+        }))
+        .pipe(gulp.dest("./app/scss/"));
+}
